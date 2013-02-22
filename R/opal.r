e326d2108596e37ba6ea4a99aa94d11a9c994332
Yannick Marcon <yannick.marcon@obiba.org>
2013-02-22 17:15:07 -0500

    Http status message added.

--- R/opal.r
+++ R/opal.r
@@ -263,11 +263,11 @@ opal.rm <- function(opal, symbol) {
 #' @keywords internal
 .handleResponse <- function(response) {
   if(response$code >= 400) { 
-    msg <- ""
+    msg <- gsub("[\n\r]","",response$headers['statusMessage'])
     if(response$code < 500) {
-      msg <- paste("Invalid request (", response$code, ")", sep='')
+      msg <- paste(msg, " (", response$code, ")", sep='')
     } else {
-      msg <- paste("Server error (", response$code, ")", sep='')
+      msg <- paste(msg, " (", response$code, ")", sep='')
     }
     if (!.isContentEmpty(response$content)) {
       msg <- paste(msg, ":", response$content)
