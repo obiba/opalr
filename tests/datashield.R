85835969b0689841471d33d06b502c324e226b91
Yannick Marcon <yannick.marcon@obiba.org>
2013-02-25 09:57:28 -0500

    some test files added.

---
+++ tests/datashield.R
@@ -0,0 +1,28 @@
+#!/usr/bin/env Rscript
+
+#
+# Opal Datashield client
+#
+
+library(opal)
+# http login
+#o<-opal.login('dsuser', 'password', 'http://localhost:8080')
+# https login with ssl options
+o1<-opal.login('dsuser1', 'password', 'https://some.opal.host:8443',opts=list(ssl.verifyhost=0,ssl.verifypeer=0,sslversion=3))
+o2<-opal.login('dsuser2', 'password', 'https://some.other.opal.host:8443',opts=list(ssl.verifyhost=0,ssl.verifypeer=0,sslversion=3))
+opals<-list(o1, o2)
+
+message("**** assign some variables:")
+datashield.assign(opals,'SEX','opal-data.HOP:GENDER')
+datashield.assign(opals,'BMI','opal-data.HOP:PM_BMI_CONTINUOUS')
+datashield.symbols(opals)
+
+message("**** execute some summary (if these methods are available in the opals):")
+datashield.aggregate(opals,'length(SEX)')
+datashield.aggregate(opals,'summary(SEX)')
+datashield.aggregate(opals,'summary(BMI)')
+
+message("**** clean symbols:")
+datashield.rm(opals,'SEX')
+datashield.rm(opals,'BMI')
+datashield.symbols(opals)
---
+++ tests/opal-datasources.R
@@ -0,0 +1,41 @@
+#!/usr/bin/env Rscript
+
+#
+# Opal R client
+#
+
+library(opal)
+# http login
+#o<-opal.login('administrator', 'password', 'http://localhost:8080')
+# https login with ssl options
+o<-opal.login('administrator', 'password', 'https://localhost:8443',opts=list(ssl.verifyhost=0,ssl.verifypeer=0,sslversion=3))
+
+message("**** datasources:")
+opal.datasources(o)
+message("**** opal-data datasource:")
+opal.datasource(o,'opal-data')
+message("**** opal-data datasource tables:")
+opal.tables(o,'opal-data')
+message("**** opal-data datasource HOP table:")
+opal.table(o,'opal-data','HOP')
+message("**** opal-data datasource HOP table GENDER variables:")
+opal.variables(o,'opal-data','HOP')
+message("**** opal-data datasource HOP table GENDER variable:")
+opal.variable(o,'opal-data','HOP','GENDER')
+message("**** missing variable:")
+opal.variable(o,'opal-data','HOP','XXXX')
+
+message("**** assign some variables:")
+opal.assign(o,'SEX','opal-data.HOP:GENDER')
+opal.assign(o,'BMI','opal-data.HOP:PM_BMI_CONTINUOUS')
+opal.symbols(o)
+
+message("**** execute some summary:")
+opal.execute(o,'length(SEX)')
+opal.execute(o,'summary(SEX)')
+opal.execute(o,'summary(BMI)')
+
+message("**** clean symbols:")
+opal.rm(o,'SEX')
+opal.rm(o,'BMI')
+opal.symbols(o)
