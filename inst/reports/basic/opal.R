5f2e7b7d2c8cf8c55fb109ab4ae3eecde28fee99
Yannick Marcon <yannick.marcon@obiba.org>
2014-07-18 12:07:14 -0400

    opal report tutotrial added.

---
+++ inst/reports/basic/opal.R
@@ -0,0 +1,24 @@
+# Load the required libraries on client side and login in Opal
+library(opal)
+o <- opal.login('administrator', 'password', 'http://demo.obiba.org:8080')
+
+# Assign some variables into a data.frame with associated *D* symbol in R on Opal server side:
+opal.assign(o,'D','mica_demo.FNAC',variables=c('SVUOSI','SUKUP','PITUUS','PAINO'))
+
+# Preview the assigned data.frame:
+opal.execute(o,'head(D)')
+
+#Summary of the assigned data.frame:
+opal.execute(o,'summary(D)')
+
+# Histogram figure of the *PITUUS* variable:
+plot(opal.execute(o,'hist(D$PITUUS)'))
+
+# Loading *ggplot2* library in R on Opal server side... This will fail if *ggplot2* is not installed in R server environment.
+opal.execute(o, 'library(ggplot2)')
+
+# Plot *PITUUS* vs. *PAINO* with "lm" smoothing:
+opal.execute(o,'qplot(PITUUS,PAINO, data=D) + geom_smooth(method="lm")')
+
+# Cleaning the resources on Opal server side...
+opal.logout(o)
