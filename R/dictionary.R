3becd40e45dc395314c4b6b2957fa8c0148fc21b
Yannick Marcon <yannick.marcon@obiba.org>
2021-03-11 14:25:50 +0100

    Check for IDs in dictionary.inspect

--- R/dictionary.R
+++ R/dictionary.R
@@ -182,6 +182,10 @@ dictionary.inspect <- function(tibble, id.name = 'id', warn = TRUE) {
   if (!(id.name %in% colnames(tibble))) {
     stop("No identifiers column with name '", id.name, "'", call. = FALSE)
   }
+  # check for non missing ID values, mandatory
+  if (!all(sapply(tibble[[id.name]], function(id) !(is.null(id) || is.na(id) || nchar(as.character(id)) == 0)))) {
+    stop("Some identifiers are missing in column '", id.name, "'", call. = FALSE)
+  }
   rval <- TRUE
   # check whether it is a multilines dataset
   if (length(unique(tibble[[id.name]])) < nrow(tibble)) {
