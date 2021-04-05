936498a8266d05a604cb76f8379456f0c02c4036
Yannick Marcon <yannick.marcon@obiba.org>
2021-04-05 18:12:49 +0200

    sql on any datasources added

--- tests/testthat/test-sql.R
+++ tests/testthat/test-sql.R
@@ -1,16 +1,36 @@
-test_that("Table SQL", {
+test_that("Table SQL with project context", {
   check_skip()
   #skip("Temporary skip")
   o <- opal.login("administrator", "password")
   
-  df <- opal.sql(o, 'CNSIM', 'select avg(LAB_HDL) as HDL_AVG, GENDER from CNSIM1 where LAB_HDL is not null group by GENDER')
+  df <- opal.sql(o, 'select avg(LAB_HDL) as HDL_AVG, GENDER from CNSIM1 where LAB_HDL is not null group by GENDER', 'CNSIM')
   expect_equal(df$HDL_AVG, c(1.5170, 1.6223))
   expect_equal(df$GENDER, c("0", "1"))
   
   # table not found
-  expect_error(opal.sql(o, 'CNSIM', 'select * from CNSIM4'))
+  expect_error(opal.sql(o, 'select * from CNSIM4', 'CNSIM'))
   # SQL syntax error
-  expect_error(opal.sql(o, 'CNSIM', 'select from CNSIM1'))
+  expect_error(opal.sql(o, 'select from CNSIM1', 'CNSIM'))
+  
+  # check no R session created
+  expect_null(o$rid)
+  
+  opal.logout(o)
+})
+
+test_that("Table SQL without project context", {
+  check_skip()
+  #skip("Temporary skip")
+  o <- opal.login("administrator", "password")
+  
+  df <- opal.sql(o, 'select avg(LAB_HDL) as HDL_AVG, GENDER from `CNSIM.CNSIM1` where LAB_HDL is not null group by GENDER')
+  expect_equal(df$HDL_AVG, c(1.5170, 1.6223))
+  expect_equal(df$GENDER, c("0", "1"))
+  
+  # table not found
+  expect_error(opal.sql(o, 'select * from `CNSIM.CNSIM4`'))
+  # SQL syntax error
+  expect_error(opal.sql(o, 'select from `CNSIM.CNSIM1`'))
   
   # check no R session created
   expect_null(o$rid)
